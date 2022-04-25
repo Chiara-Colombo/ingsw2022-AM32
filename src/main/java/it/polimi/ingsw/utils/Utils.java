8eb92cb56afa78039d57dd2d6e0ccb79f5a750b6
Riccardo-Corti <riccardo2.corti@mail.polimi.it>
2022-04-25 17:42:55 +0200

    pushed code

---
+++ src/main/java/it/polimi/ingsw/utils/Utils.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.utils;
+
+public class Utils {
+    public static final String CARDS_RESOURCE_PATH = "src\\main\\resources\\assistantCards.json";
+    public static final int DEFAULT_SERVER_PORT = 30300;
+    public static final String DEFAULT_SERVER_ADDRESS = "localhost";
+}
