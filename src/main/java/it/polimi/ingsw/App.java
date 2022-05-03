3b0e6012c9cbbba761cda6206b3eaff4cd3a1f51
Riccardo-Corti <riccardo2.corti@mail.polimi.it>
2022-05-03 10:03:33 +0200

    Basic client/server connection code

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,13 +1,25 @@
 package it.polimi.ingsw;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import it.polimi.ingsw.client.CLI;
+import it.polimi.ingsw.server.Server;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Menù\n\n 1 - Start Server\n 2 - Start CLI Client\n\n- ");
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            try {
+                new Server().start();
+            } catch (IOException e) {
+                System.out.println("Error - Can't start the server");
+            }
+        } else if (choice == 2) {
+            CLI client = new CLI();
+            client.start();
+        }
     }
 }
