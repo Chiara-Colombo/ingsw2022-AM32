4e89d89391231881da7f8e8eb27c0a59b702f884
Chiara-Colombo <chiara14.colombo@mail.polimi.it>
2022-07-01 10:32:23 +0200

    Java DOC

--- src/main/java/it/polimi/ingsw/server/ConcreteServerVisitor.java
+++ src/main/java/it/polimi/ingsw/server/ConcreteServerVisitor.java
@@ -10,14 +10,27 @@ public class ConcreteServerVisitor implements VisitorServer{
     private final ClientHandler player;
     private final MatchManager matchManager;
 
+    /**
+     * Class Constructor
+     */
+
     public ConcreteServerVisitor(MatchManager matchManager, ClientHandler player) {
          this.matchManager = matchManager;
          this.player = player;
     }
 
+    /**
+     * Setter for the Server Controller
+     * @param serverController the Server Controller
+     */
+
     public void setServerController(ServerController serverController) {
         this.serverController = serverController;
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void visitMessage(NumOfPlayersResponse numOfPlayersResponse) {
@@ -26,6 +39,10 @@ public class ConcreteServerVisitor implements VisitorServer{
         } else
             this.serverController.setNumOfPlayers(numOfPlayersResponse.getNumOfPlayers(),player);
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void visitMessage(GameModeResponse gameModeResponse)  {
@@ -36,65 +53,117 @@ public class ConcreteServerVisitor implements VisitorServer{
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
+
     @Override
     public void visitMessage(SetUsername setUsername)  {
        this.serverController.setUsername(setUsername.getUsername(), player);
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void visitMessage(AssistantCardResponse assistantCardResponse) {
         this.serverController.setAssistantCard(assistantCardResponse.getChosenCard());
     }
 
+    /**
+     * {@inheritDoc}
+     */
+
     @Override
     public void visitMessage(CloudResponse cloudResponse) {
         this.serverController.chooseCloud(cloudResponse.getCloudIndex());
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void visitMessage(MatchResponse matchResponse) throws IOException {
         this.matchManager.matchResponse(matchResponse.isNewMatch(), this.player);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+
     @Override
     public void visitMessage(MoveMNResponse moveMNResponse) {
         this.serverController.moveMotherNature(moveMNResponse.getPosition());
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void visitMessage(MovePawnResponse movePawnResponse) {
         this.serverController.moveStudent(movePawnResponse.getStudentIndex(), movePawnResponse.getIslandIndex(), movePawnResponse.isMoveOnSchoolBoard());
     }
 
+    /**
+     * {@inheritDoc}
+     */
+
     @Override
     public void visitMessage(SelectColorResponse selectColorResponse) {
         this.serverController.selectColor(selectColorResponse.getColor());
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void visitMessage(SelectIslandResponse selectIslandResponse) {
         this.serverController.selectIsland(selectIslandResponse.getIslandIndex());
     }
 
+    /**
+     * {@inheritDoc}
+     */
+
     @Override
     public void visitMessage(SelectPawnResponse selectPawnResponse) {
         this.serverController.selectPawn(selectPawnResponse.getPawnIndex());
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void visitMessage(UseCharacterCard useCharacterCard) {
         this.serverController.useCharacterCard(useCharacterCard.getCharacter());
     }
 
+    /**
+     * {@inheritDoc}
+     */
+
     @Override
     public void visitMessage(WizardCardResponse wizardCardResponse) {
         this.serverController.setWizard(wizardCardResponse.getChosenWizard());
     }
 
+    /**
+     * {@inheritDoc}
+     */
+
     @Override
     public void visitMessage(SelectPawnsResponse selectPawnsResponse) {
         this.serverController.selectPawns(selectPawnsResponse.getPawnsindex());
     }
+
+    /**
+     * {@inheritDoc}
+     */
 
     @Override
     public void visitMessage(SelectColorsResponse selectColorsResponse) {
--- src/main/java/it/polimi/ingsw/server/VisitorServer.java
+++ src/main/java/it/polimi/ingsw/server/VisitorServer.java
@@ -10,26 +10,113 @@ import java.io.IOException;
 public interface VisitorServer {
 
     /**
-     * Method that sets the Number of players according to NumOfPlayersResponse
+     * Method that gets the Number of players according to NumOfPlayersResponse
      * if the input is valid
      * @param numOfPlayersResponse response that contains the number of player chosen by the first user
      */
 
     void visitMessage(NumOfPlayersResponse numOfPlayersResponse);
 
+    /**
+     * Method that gets the Game mode according to the GameModeResponse
+     * @param gameModeResponse response that contains the Game mode (expert mode or simple mode)
+     */
+
     void visitMessage(GameModeResponse gameModeResponse);
+
+    /**
+     * Method that gets the username of a player
+     * @param username the response that contains the username
+     */
+
     void visitMessage(SetUsername username);
+
+    /**
+     * Method that gets the Assistant Card chosen by a player
+     * @param assistantCardResponse message that contains the chosen assistant card
+     */
+
     void visitMessage(AssistantCardResponse assistantCardResponse);
+
+    /**
+     * Method that chooses the cloud according to the CloudResponse
+     * @param cloudResponse message that contains the chosen Cloud
+     */
+
     void visitMessage(CloudResponse cloudResponse);
+
+    /**
+     * Method that get if the player wants to create a new match
+     * @param matchResponse message that contains the fact that the player wants to create a new match
+     *                      or participate an existing match
+     * @throws IOException
+     */
+
     void visitMessage(MatchResponse matchResponse) throws IOException;
+
+    /**
+     * Method that moves Mother Nature according to moveMNResponse
+     * @param moveMNResponse message that contains the position where the player want to put Mother Nature
+     */
+
     void visitMessage(MoveMNResponse moveMNResponse);
+
+    /**
+     * Method that moves a Pawn according to MovePawnResponse
+     * @param movePawnResponse message that contains the which pawn the player wants to move
+     *                         and where he wants to put the pawn
+     */
+
     void visitMessage(MovePawnResponse movePawnResponse);
+
+    /**
+     * Method that get the color chosen by the player
+     * @param selectColorResponse message that contains the chosen color
+     */
+
     void visitMessage(SelectColorResponse selectColorResponse);
+
+    /**
+     * Method that gets the island chosen by the player
+     * @param selectIslandResponse message that contains tha chosen islands
+     */
+
     void visitMessage(SelectIslandResponse selectIslandResponse);
+
+    /**
+     * Method that gets the pawn selected by a player
+     * @param selectPawnResponse message that contains the pawn selected by the player
+     */
+
     void visitMessage(SelectPawnResponse selectPawnResponse);
+
+    /**
+     * Method that gets the Character Card that the player want to use
+     * @param useCharacterCard message that contains the selected Character Card
+     */
+
     void visitMessage(UseCharacterCard useCharacterCard);
+
+    /**
+     * Method that gets the Wizard Card that the player want to use
+     * @param wizardCardResponse message that contains the selected Wizard Card
+     */
+
     void visitMessage(WizardCardResponse wizardCardResponse);
+
     void visitMessage(Quit quit);
+
+    /**
+     * Method that gets the pawns selected by a player
+     * @param selectPawnsResponse  message that contains the pawns selected by the player
+     */
+
     void visitMessage(SelectPawnsResponse selectPawnsResponse);
+
+    /**
+     * Method that get the color chosen by the player
+     * @param selectColorsResponse message that contains the chosen color
+     */
+
     void visitMessage(SelectColorsResponse selectColorsResponse);
 }
